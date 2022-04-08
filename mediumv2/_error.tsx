dac3d653a1a19764f77f3ef06093dd9af77e7546
blackmouse572 <ngocvlqt1995@gmail.com>
2022-04-08 20:54:28 +0700

    Add error pages

--- mediumv2/_error.tsx
+++ mediumv2/_error.tsx
@@ -1,13 +1,16 @@
-import Error from 'next/error';
-
-
-function Page({ statusCode }:any) {
-  return <Error statusCode={statusCode}></Error>;
-}
-
-Page.getInitialProps = ({ res, err }:{res:any; err:any}) => {
-  const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
-  return { statusCode };
-};
-
-export default Page;
+function Error({ statusCode }:any) {
+    return (
+      <p>
+        {statusCode
+          ? `An error ${statusCode} occurred on server`
+          : 'An error occurred on client'}
+      </p>
+    )
+  }
+  
+  Error.getInitialProps = ({ res, err }:{res:any; err:any}) => {
+    const statusCode = res ? res.statusCode : err ? err.statusCode : 404
+    return { statusCode }
+  }
+  
+  export default Error
